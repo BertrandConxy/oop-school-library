35fbf5f2ff80ead59c7f4400256c3522536c6eca
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-04-12 16:02:28 +0200

    Add association between classroom and student

--- student.rb
+++ student.rb
@@ -1,5 +1,5 @@
 require './person'
-
+require './classroom'
 class Student < Person
   attr_reader :classroom
 
@@ -11,4 +11,9 @@ class Student < Person
   def play_hooky
     "¯\(ツ)/¯"
   end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+    end
 end

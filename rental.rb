42f17218ff91a46e11b5e7c6fd9d3a8dad89c756
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-04-12 16:49:41 +0200

    Correct linter errors

--- rental.rb
+++ rental.rb
@@ -1,13 +1,14 @@
 class Rental
-    attr_reader :book, :person
-    attr_accessor :date
-    def initialize(date, book, person)
-        @date = date
+  attr_reader :book, :person
+  attr_accessor :date
 
-        @book = book
-        book.rentals << self
+  def initialize(date, book, person)
+    @date = date
 
-        @person = person
-        person.rentals << self
-    end
+    @book = book
+    book.rentals << self
+
+    @person = person
+    person.rentals << self
+  end
 end

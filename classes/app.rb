d41b791b17eb304957d600358765132a92810ba2
zairdon20 <josphat2020@gmail.com>
2022-04-19 11:28:54 +0200

    Add bookmodules.rb>> update the app.rb

--- classes/app.rb
+++ classes/app.rb
@@ -1,6 +1,6 @@
 require_relative './student'
+require_relative './bookmodule'
 require_relative './classroom'
-require_relative './book'
 require_relative './rental'
 require_relative './teacher'
 require_relative './person'
@@ -8,7 +8,7 @@ require_relative './decorator'
 
 class App
   def initialize
-    @books = []
+    @books = bookmodule.new 
     @people = []
     @rentals = []
   end
@@ -30,13 +30,13 @@ class App
     selection = gets.chomp
     case selection
     when '1'
-      list_all_books
+      @books.list_all_books 
     when '2'
       list_all_people
     when '3'
       create_person
     when '4'
-      create_book
+      @books.create_book 
     when '5'
       create_rental
     when '6'
@@ -50,13 +50,6 @@ class App
     option
   end
 
-  def list_all_books
-    puts 'Sorry, there are no books available at the moment' if @books.empty?
-    puts "There are #{@books.count} book(s) in the system"
-    @books.each_with_index do |book, index|
-      puts "#{index + 1}) Book Title: \"#{book.title}\" | Author: #{book.author}"
-    end
-  end
 
   def list_all_people
     puts 'Sorry, there are no people available at the moment' if @people.empty?
@@ -114,22 +107,6 @@ class App
     puts "Student is created successfully. Your ID is #{student.id}"
   end
 
-  def create_book
-    print 'Enter the book title: '
-    title = gets.chomp
-    print 'Enter the book author: '
-    author = gets.chomp
-
-    if title.strip != '' && author.strip != ''
-      book = Book.new(title, author)
-      @books << book
-      puts ''
-      puts 'Book is created successfully'
-    else
-      puts ''
-      puts 'Please provide title and author of the book'
-    end
-  end
 
   def create_rental
     if @books.empty? && @people.empty?

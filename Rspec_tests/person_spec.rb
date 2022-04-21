5a25513249c470c77d5b165237729ee608ddc648
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-04-21 11:33:20 +0200

    test person class

--- Rspec_tests/person_spec.rb
+++ Rspec_tests/person_spec.rb
@@ -1,24 +1,37 @@
 require_relative '../classes/person'
+require_relative '../classes/rental'
+require_relative '../classes/book'
 
 
 describe Person do
     before :each do
-        @person = Person.new('Business ethics', 'Robert')
+        @person = Person.new(15, 'Kandy Boy')
     end
-    context 'given two arguments' do
+    context 'given arguments' do
         it "instantiate an object " do
             expect(@person).to be_instance_of(Person)
         end
 
-        it "instatiate object with two properties" do
-            expect(@person.title).to eql('Business ethics')
-            expect(@person.author).to eql('Robert')
-        end
-
-        it "add a rental" do
-            @student = Student.new('s1', 12, 'bertrand')
-            expect(@book.rentals.length).to eql(0)
-            @book.add_rental('2022-03-03', @student)
-            expect(@book.rentals.length).to eql(1)
+        it "instatiate object with all person properties" do
+            expect(@person.id)
+            expect(@person.name).to eql('Kandy Boy')
+            expect(@person.age).to eql(15)
+            expect(@person.parent_permission).to be true
         end
     end
+
+    context 'when can_use_service? method is called' do
+        it "return true or false" do
+            expect(@person.can_use_services?).not_to be false
+        end
+    end
+
+    context "when add rental method is callled" do
+        it "add the rental to the array" do
+            expect(@person.rentals.length).to eql(0)
+            @book = Book.new('Kill it', 'man')
+            @person.add_rental('2022-02-03', @book)
+            expect(@person.rentals.length).to eql(1)
+        end
+    end
+end
